4ca9373d91e29b612faa8eddd5a6b0cfe6bf1307
Paulina.P <pauline.uxd@gmail.com>
2019-04-08 21:15:42 +0200

    vue_first_steps

---
+++ vue_js/script.js
@@ -0,0 +1,22 @@
+var data = {
+  "name": "Extremely Affordable on-the-road hotel",
+  "description": "You'll be thrilled when visiting this extremely cool hotel, wherever you're going, no matter how tired you are, come on in, and enjoy the experience",
+  "image": "https://source.unsplash.com/random",
+  "price": 99
+}
+
+
+
+
+
+
+
+
+
+
+
+var app = new Vue ({
+  el: '#app',
+  data: data
+  }
+);
