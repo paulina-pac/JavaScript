ec8aa65f1dccade3f7f03a637810dcf28f721211
Paulina.P <pauline.uxd@gmail.com>
2019-04-09 23:01:55 +0200

    Js_rewind

--- vue_js/script.js
+++ vue_js/script.js
@@ -9,14 +9,67 @@ var data = {
 
 
 
-
-
-
-
-
-
-var app = new Vue ({
+var app = new Vue({
   el: '#app',
   data: data
-  }
+}
 );
+
+
+
+// ------------------------------------------ JS VARIABLES and TYPES -----------------------------------------
+
+var box = "blue", 
+smallBox = "smallie", 
+tinyBox = "tiny", 
+longBox = "longie";
+
+// ------------------------------------------ JS VARIABLES and TYPES -----------------------------------------
+
+// A string is character data meaning things like sentences, words, single letters, numbers, or punctuation. 
+// Basically it means anything between quote marks.
+
+"this is a string";
+
+"This is Joe's \"favourite\" string";
+
+"This is \
+Joe's \
+favourite string";
+
+// ------------------------------------------ String Properties and methods -----------------------------------------
+
+var myString = "This is my string. Leave it alone";
+
+myString.length;
+
+myString.toUpperCase;
+
+
+// ------------------------------------------ NUMBERS -----------------------------------------
+
+1;
+1.9;
+12.334343434455555555555555555555555553333;
+-12;
+Infinity;
+-Infinity;
+NaN;
+
+var myNumber = 33;
+
+Math.round(12.988338383844995);
+
+Math.random();
+
+// ------------------------------------------ BOOLEANS - TRUE OR FALSE  -----------------------------------------
+
+true;
+false;
+
+var myLocation = "room", myOtherlocation = "bathroom";
+
+myLocation === myOtherlocation;
+
+
+
